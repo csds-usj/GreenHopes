0d59f2f5fd4767e0aef1a10b74c3cfaf28c31e10
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2025-07-30 15:22:29 +0530

    Revert "add: nav and footer"
    This reverts commit aa2776c07a08220d139a006b9484752b6c22f23e.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,12 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return (
-    <>
-      <main>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is a simple home component.</p>
-      </main>
-    </>
-  );
+  return <Welcome />;
 }
